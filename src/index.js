7da2c17021bebabd37ebb7f804e02ea652f47dd5
tanishka121 <tanishkachauhan121@gmail.com>
2025-10-18 23:23:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio_tanishka">
+    <BrowserRouter basename="/live-streaming">
       <App />
     </BrowserRouter>
     
